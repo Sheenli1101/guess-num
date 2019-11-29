9ee78cafe095cc2920fd281732899a3ec0b82457
Sheen Li <bingxun1101@gmail.com>
2019-11-29 10:32:24 +0800

    second commit

--- guess-num.py
+++ guess-num.py
@@ -1,13 +1,16 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count + 1
 	num = input("請輸入數字:")
 	num = int(num)
+	print("這是你猜的第", count, "次")		
 	if num == r:
 		print("你猜中了!")
 		break
 	elif num > r:
 		print("比答案大")
 	elif num < r:
-		print("比答案小")	
+		print("比答案小")
